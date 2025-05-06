50319237a9058e75a17c83f3e81761d9158a1ef9
Sheayoo <lmdwyzh@163.com>
2025-05-07 07:31:13 +0800

    first

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,13 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 500,
   name: 'Subscription',
   version: 0,
   author: 'author',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri:
+    'https://fastly.jsdelivr.net/gh/lmdwyzh/sheayoo@main/dist/gkd.json5 ',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
