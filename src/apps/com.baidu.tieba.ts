b8695325db8b76ad9961b751e66a86dac3ba7f85
Sheayoo <lmdwyzh@163.com>
2025-05-07 08:07:32 +0800

    3

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -3,5 +3,44 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.baidu.tieba',
   name: '贴吧',
-  groups: [],
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      actionMaximumKey: 0,
+      priorityTime: 10000,
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          matches: 'TextView[text^="跳过"][text.length<=10]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12775906',
+            'https://i.gkd.li/i/12566191',
+            'https://i.gkd.li/i/12870916',
+            'https://i.gkd.li/i/13233500',
+          ],
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          anyMatches: [
+            '@View[text=null][clickable=true][childCount=0][visibleToUser=true][width<200&&height<200] +(1,2) TextView[index=parent.childCount.minus(1)][text=null] <n FrameLayout[childCount>2] >(7,8,9,10) [text*="第三方应用" || text*="扭动手机" || text*="点击或上滑"][visibleToUser=true]',
+            'FrameLayout > FrameLayout[childCount>2] > @View[text=null][clickable=true][childCount=0][visibleToUser=true][width<200&&height<200] +(1,2) TextView[index=parent.childCount.minus(1)][text=null][visibleToUser=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/i/13322227',
+        },
+        {
+          key: 2,
+          matches:
+            'TextView[text="广告"] - @LinearLayout[clickable=true] > [text="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/i/13168386',
+        },
+      ],
+      order: -10,
+    },
+  ],
 });
